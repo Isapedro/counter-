1d1c6e2cb8eb4e9ba8d50ce9c824295ae214416c
Isapedro <isa.pedro92@hotmail.com>
2023-12-05 19:25:35 +0100

    adding comments for different ways of doing

---
+++ other.js
@@ -0,0 +1,32 @@
+// let count= 0 
+
+
+// Better way of doing because is two functions basicly doing the same thing:
+// functionadjustCount(amount){
+  //count = count + amount;
+  //document.getElementbyId("counter").innerText = count;
+  //}
+// on the button it will be the name of the function but in the incremented one with the paramenter of 1
+// on the decremented button there will be -1 inside the brakets( which means parameter)
+
+
+
+
+// function incrementCount(){
+  // count= count + 1;
+  // document.get.ElementById("value").innerText = count;
+//}
+
+
+
+// function decrementCount(){
+  // count= count  - 1;
+  // document.get.ElementById("value").innerText = count;
+//}
+
+
+
+// function resetCount(){
+  // count= 0;
+  // document.get.ElementById("value").innerText = count;
+//}
